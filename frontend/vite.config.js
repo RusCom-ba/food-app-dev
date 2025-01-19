a94f2b95f3c6e78cc566120b8017aa15f208fc52
Rusmir <rusmir.c@hotmail.com>
2025-01-19 17:52:01 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx'],  // Osiguraj da JSX ekstenzije budu prepoznate
-  },
   build: {
-    outDir: 'dist',  // Osiguraj da build ide u dist folder
+    outDir: 'dist',  
+    emptyOutDir: true,  
   },
-  esbuild: {
-    loader: 'jsx',  // Ispravan loader za JSX fajlove
-  }
 });
